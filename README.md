2b4ac377d772c375509d8debcfd6c64c733a264b
Shkoh-Hamasoor <shkoh.hamasoor@gmail.com>
2021-04-11 01:38:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nav-bar-project-5ysqtd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nav-bar-project-5ysqtd)
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.css"]
+})
+export class NavigationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
