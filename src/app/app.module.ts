9c532a5c80b5b3d2c51ffa3a7b536f743d87051c
Shkoh-Hamasoor <shkoh.hamasoor@gmail.com>
2021-04-15 14:55:51 -0500

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavigationComponent ],
+  declarations: [ AppComponent, HelloComponent, NavigationComponent, HomepageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
