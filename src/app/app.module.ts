110e33ba4272aa351bbd65f3ac7ef7e80b383d87
Shkoh-Hamasoor <shkoh.hamasoor@gmail.com>
2021-04-16 15:47:56 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {AngularFireModule} from '@angular/fire';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AngularFireModule.initializeApp({
+    apiKey: "AIzaSyDqfLrI75DERzid7Ij3EeYcT3KFb9TkUpg",
+    authDomain: "login-logout-1d9da.firebaseapp.com",
+    projectId: "login-logout-1d9da",
+    storageBucket: "login-logout-1d9da.appspot.com",
+    messagingSenderId: "998851211896",
+    appId: "1:998851211896:web:65aba3fb721deb9792338d"
+  })
+  ],
   declarations: [ AppComponent, HelloComponent, NavigationComponent, HomepageComponent ],
   bootstrap:    [ AppComponent ]
 })
